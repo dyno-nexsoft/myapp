0d136397b8a96ec420228f023caae5e7d0c5cdc8
dyno-nexsoft <dyno-nexsoft@users.noreply.github.com>
2025-03-10 06:15:09 +0000

    deploy: b059e039bccf831d0672e1eabde135a6dea4b687

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "945627222"
+    serviceWorkerVersion: "2279414226"
   }
 });
